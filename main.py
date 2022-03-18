a56c6da5d0eede9388a14a04c9d292aa006bf0a3
Zinoviy Kuzmitch <93143550+DedZinoviy@users.noreply.github.com>
2022-03-18 16:34:30 +0300

    Улучшена читаемость кода
    Вычисление в функции electicScheme3var разбито на несколько действий для лучшей читаемости.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from probablity import Probability
 
 def electicScheme3var(elementProbabilityList):
-    return elementProbabilityList[0] + Probability.negativeProbability(elementProbabilityList[0]) *((elementProbabilityList[1] + Probability.negativeProbability(elementProbabilityList[1]) * elementProbabilityList[2] * elementProbabilityList[3]) * (elementProbabilityList[4] + Probability.negativeProbability(elementProbabilityList[4]) * elementProbabilityList[5]))
+    act_1 = elementProbabilityList[1] + Probability.negativeProbability(elementProbabilityList[1]) * elementProbabilityList[2] * elementProbabilityList[3]
+    act_2 = elementProbabilityList[4] + Probability.negativeProbability(elementProbabilityList[4]) * elementProbabilityList[5]
+    return elementProbabilityList[0] + Probability.negativeProbability(elementProbabilityList[0]) *(act_1 * act_2)
 
 float_list = []
 for element in input().split():
