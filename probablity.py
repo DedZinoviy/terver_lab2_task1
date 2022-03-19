932920713665ecf4c689f15d08c9068014dc9ee1
Дмитрий Деменков <demon.krut2002@mail.ru>
2022-03-19 10:22:15 +0300

    new beta

--- probablity.py
+++ probablity.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from combinatoric import Combinatoric
 
 class Probability:
@@ -10,17 +9,4 @@ class Probability:
         return 1 - probability
 
     def bernoulli(p, n, m):
-=======
-from combinatoric import Combinatoric
-
-class Probability:
-    def __init__(self):
-        pass
-
-
-    def negativeProbability(probability):
-        return 1 - probability
-
-    def bernoulli(p, n, m):
->>>>>>> 18294e9de50f832036c408dfa4b24bea2a46a6bf
         return Combinatoric.combinations_without_repeats(n, m) * p**m * Probability.negativeProbability(p)**(n-m)
