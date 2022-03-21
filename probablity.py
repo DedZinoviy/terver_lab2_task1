167541bc41e8a54ad7ab2a1d132f515104f30eb5
Zinoviy Kuzmitch <93143550+DedZinoviy@users.noreply.github.com>
2022-03-21 19:48:31 +0300

    Добавление новых функций.
    Добавлены функции для вычисления полной вероятности и формулы Байеса.

--- probablity.py
+++ probablity.py
@@ -10,3 +10,12 @@ class Probability:
 
     def bernoulli(p, n, m):
         return Combinatoric.combinations_without_repeats(n, m) * p**m * Probability.negativeProbability(p)**(n-m)
+    
+    def fullProbability(hypothesisList, probabilityList):
+        resultProbability = 0
+        for i in range(len(hypothesisList)):
+            resultProbability += hypothesisList[i] * probabilityList[i]
+        return resultProbability
+
+    def bayesFormula(neededHypotesisIndex, hypothesisList, probabilityList):
+            return hypothesisList[neededHypotesisIndex] * probabilityList[neededHypotesisIndex] / Probability.fullProbability(hypothesisList, probabilityList)
